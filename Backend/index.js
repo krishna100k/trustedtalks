77f914f5908038e5d4da88824a1835be54e5c541
krishna10k <krishnaprasadawala@gmail.com>
2023-12-10 00:12:45 +0530

    new Backend

--- Backend/index.js
+++ Backend/index.js
@@ -4,18 +4,22 @@ import userRoutes from "./Users.js"
 import chatRoutes from "./chats.js"
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import serverless from "serverless-http"
+
 
 const app = express();
-const PORT = 3000;
-app.use(bodyParser.json())
+// const PORT = 3000;
+app.use(bodyParser.json());
 
 mongoose.connect("mongodb+srv://krishna:Krishna0870@chat.zimavbi.mongodb.net/")
 app.use(cors())
 app.use('/user', userRoutes )
 app.use("/chats", chatRoutes);
 
-app.get('/', (req, res)=>{
-    res.send("Hello From Homepage")
+app.get('/home', (req, res)=>{
+    res.send("Database Connected")
 })
 
-app.listen(PORT, ()=>console.log(`Listenting to port on http://localhost:${PORT}`));
+// app.listen(PORT, ()=>console.log(`Listenting to port on http://localhost:${PORT}`));
+
+export const handler = serverless(app)
